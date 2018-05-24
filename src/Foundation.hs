995cb1bd4f5d91b8c6b6de9455a2fc5a5b8055ae
Nicholas Scheel <monoidmusician@gmail.com>
2018-05-24 16:35:51 -0500

    Add example page (Functor)

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -116,6 +116,11 @@ instance Yesod App where
                     , menuItemAccessCallback = True
                     }
                 , NavbarLeft $ MenuItem
+                    { menuItemLabel = "Functor (fp)"
+                    , menuItemRoute = FPFunctorR
+                    , menuItemAccessCallback = True
+                    }
+                , NavbarLeft $ MenuItem
                     { menuItemLabel = "Profile"
                     , menuItemRoute = ProfileR
                     , menuItemAccessCallback = isJust muser
@@ -166,6 +171,7 @@ instance Yesod App where
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
+    isAuthorized FPFunctorR _ = return Authorized
 
     -- the profile route requires that the user is authenticated, so we
     -- delegate to that function
